75e8a0b069d536fa601c3c0c55ade2ac3a5005ef
Amit Singh <smamit27@gmail.com>
2016-11-22 23:00:12 +0530

    New App

---
+++ src/js/app.js
@@ -0,0 +1,16 @@
+var sapientApp = angular.module('sapientApp',['ui.router']);
+
+sapientApp.config(function($stateProvider, $urlRouterProvider) {
+    
+    $urlRouterProvider.otherwise('/sapient');
+    
+    $stateProvider
+        
+        .state('sapient', {
+            url: '/sapient',
+            templateUrl: 'views/sapient.html',
+            controller :'sapientCtrl'
+        })
+        
+
+});
---
+++ src/js/controllers/sapientController.js
@@ -0,0 +1,6 @@
+let SapientController = function($scope){
+
+}
+
+
+angular.module('sapientApp').controller('sapientCtrl',['$scope',SapientController]);
