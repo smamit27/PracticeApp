d54d559c4f14659d4dd0f0c9131f3fd165661c01
Amit Singh <smamit27@gmail.com>
2016-11-24 23:33:48 +0530

    Date DropDown

--- src/js/controllers/sapientController.js
+++ src/js/controllers/sapientController.js
@@ -1,7 +1,29 @@
 let SapientController = function($scope){
+
+
+
+var today = new Date();
+var m_names = new Array("Jan", "Feb", "Mar", 
+"Apr", "May", "Jun", "Jul", "Aug", "Sep", 
+"Oct", "Nov", "Dec");
+
+var year = today.getFullYear();
+var month = today.getMonth();
+var date = today.getDate();
+//var datex= date + " " + m_names[month]+ " " + year;
+$scope.test = [];
+
+for(var i=0; i<30; i++){
+      var day=new Date(year, month, date - i);
+      var day1 = day.getFullYear();
+      var day2 = day.getMonth();
+      var day3 = day.getDate();
+	  $scope.form = day3 + " " + m_names[day2]+ " " + day1;
+      $scope.test.push($scope.form);
+
+
+    } 
+};
     
-
-}
-
 
 angular.module('sapientApp').controller('sapientCtrl',['$scope',SapientController]);
